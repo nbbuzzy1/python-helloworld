0737a7af40cd0ec89933cbe9db2277351e743607
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-12 17:23:28 -0400

    Using input.

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 name = 'John Smith'
 age = 20
 isNew = True
+
+name = input('What is your name? ')
+color = input('What is your favorite color ')
+print('Hi ' + name + ' your favorite color is ' + color)
