e4ed3bad8c3d43818ab103289df157d47ecfe080
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-13 18:04:49 -0400

    Python math.

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-course = 'Python for Beginners'
-print(course.find('o'))
+import math
 
-print(course.replace('Beginners', 'Absolute Beginners'))
+print(math.ceil(2.9)) #3
+print(math.floor(2.9)) #2
 
-print('Python' in course)
+x = 2.4
+print(round(x)) #2
+print(abs(-2.9)) #2.9
+
+
+
+
 
 
