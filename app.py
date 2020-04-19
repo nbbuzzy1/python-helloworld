0f919d54dfdd9846d2568108efebe84c47c738ee
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-19 17:43:06 -0400

    Finish car game.

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 user_input = ''
+started = False
 
-while user_input != 'quit':
+while True:
     user_input = input("> ").lower()
     if user_input == 'help':
         print('start - to start the car')
         print('stop - to stop the car')
         print('quit - to exit')
     elif user_input == 'start':
-        print('Car started...Ready to go!')
+        if started:
+            print('Car is already started!')
+        else:
+            print('Car started...Ready to go!')
+            started = True
     elif user_input == 'stop':
-        print('Car stopped.')
+        if not started:
+            print('Car is already stopped!')
+        else:
+            print('Car stopped.')
+            started = False
+    elif user_input == 'quit':
+        break
     else:
         print('Did not recognize that command.')
-    if user_input == 'quit':
-        break
 
 
 
