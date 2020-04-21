caa8c1e9bf9e7aabffadab22249cb3cb47265c16
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-21 17:21:42 -0400

    Nested lists.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-numbers = [1, 2, 3, 4, 5, 6, 2, 20, 3]
-largest_number = numbers[0]
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-for num in numbers:
-    if num > largest_number:
-        largest_number = num
-
-print(largest_number)
+for row in matrix:
+    for item in row:
+        print(item)
 
 
 
