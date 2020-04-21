1064352e2cec85bcaf1706beae967d15fc3c0fbd
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-21 17:28:37 -0400

    List methods.

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+numbers = [5, 2, 1, 7, 4]
+numbers2 = numbers.copy()
+numbers.append(20)
+print(numbers)
 
-for row in matrix:
-    for item in row:
-        print(item)
+numbers.insert(0, 10)
+print(numbers)
 
+numbers.remove(5)
+print(numbers)
+
+numbers.pop()
+print(numbers)
+
+print(numbers.index(7))  # or use 7 in numbers
+
+print(numbers.count(1))
+
+numbers.sort()
+numbers.reverse()
+print(numbers)
+
+numbers.clear()
+print(numbers)
+print(numbers2)
 
 
 
