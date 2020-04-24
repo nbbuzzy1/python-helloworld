adfeeb453aa6d1c28144424b98a3b7d8727fe6fa
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-24 17:36:23 -0400

    Phone mapping tutorial.

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ print(customer.get('birthdate', 'Jan 1 1980'))  # second argument is default if 
 
 print(customer.get('age'))
 
+phone = input('Phone: ')
+
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four"
+}
+
+output = ''
+
+for number in phone:
+    output += digits_mapping.get(number, "!") + ' '
+
+print(output)
+
+
 
 
 
