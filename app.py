2a07eb898725fed0f2696570b5ee5b1ff59b29d1
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-24 17:42:24 -0400

    Emoji converter

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-# dictionaries
+message = input('>')
 
-customer = {
-    "name": "John Smith",
-    "age": 30,
-    "is_verified": True
-}
+words = message.split(' ')
 
-print(customer['name'])
-
-print(customer.get('birthdate', 'Jan 1 1980'))  # second argument is default if there isn't value
-
-print(customer.get('age'))
-
-phone = input('Phone: ')
-
-digits_mapping = {
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four"
+emojis = {
+    ":)": "😊",
+    ":(": "😢"
 }
 
 output = ''
 
-for number in phone:
-    output += digits_mapping.get(number, "!") + ' '
+for word in words:
+    output += emojis.get(word, word) + ' '
 
 print(output)
 
