8ade9d1c973545396119c08342ed532d1b4d7480
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-26 12:46:27 -0400

    return statements.

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-def greet_user(first_name, last_name):
-    print(f'Hi there {first_name} {last_name}!')
-    print('Welcome aboard')
+def square(number):
+    return number * number
 
 
-print('Start')
-greet_user(last_name="Smith", first_name='John')
-print('Finish')
+squared_number = square(3)
 
-calc_cost(total=50, shipping=5, discount=0.1)
+print(squared_number)
 
-
+print(square(3))
 
