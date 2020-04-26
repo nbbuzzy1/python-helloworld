4e0d6ba10032d756444b336d5edb1da613907e2d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-26 12:34:42 -0400

    Keyword arguments.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-def greet_user(name):
-    print(f'Hi there {name}!')
+def greet_user(first_name, last_name):
+    print(f'Hi there {first_name} {last_name}!')
     print('Welcome aboard')
 
 
 print('Start')
-greet_user("John")
-greet_user("Nick")
+greet_user(last_name="Smith", first_name='John')
 print('Finish')
 
+calc_cost(total=50, shipping=5, discount=0.1)
 
 
 
