62233f9f003d5f536cf9a810cbaca2fb10a56c0b
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-04-29 17:13:20 -0400

    More class work.

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def move(self):
         print('move')
+
     def draw(self):
         print('draw')
 
 
-point1 = Point()
-point1.x = 10
-point1.y = 10
-point1.draw()
+point1 = Point(5, 10)
 print(point1.x)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f'hi I am {self.name}')
+
+
+john = Person('John Smith')
+john.talk()
+
+bob = Person('Bob Smith')
+bob.talk()
