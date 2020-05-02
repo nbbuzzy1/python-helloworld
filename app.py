cdbdf94350a59eb3cc87d53cd7d98ee666a128eb
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-05-01 20:39:14 -0400

    Using the random number generator library.

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from ecommerce.shipping import calc_shipping
-calc_shipping()
-calc_shipping()
-calc_shipping()
+import random
+
+for i in range(3):
+    print(random.randint(10, 20))
+
+
+members = ['John', 'Nick', 'Mary']
+leader = random.choice(members)
+print(leader)
+
+
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second
+
+
+dice = Dice()
+print(dice.roll())
 
